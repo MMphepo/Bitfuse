6199edd18186bf980996b247691fbedab0c833ad
MMphepo <164493503+MMphepo@users.noreply.github.com>
2026-08-04 21:46:41 +0200

    configure static files

--- backend/Bitfuse/Bitfuse/settings.py
+++ backend/Bitfuse/Bitfuse/settings.py
@@ -51,8 +51,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
+    
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -189,4 +192,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
