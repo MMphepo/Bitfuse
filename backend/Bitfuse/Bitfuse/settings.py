e89fa2f394df52c551419d7d14fe02915f00c962
MMphepo <164493503+MMphepo@users.noreply.github.com>
2026-08-04 23:04:07 +0200

    postgress database

--- backend/Bitfuse/Bitfuse/settings.py
+++ backend/Bitfuse/Bitfuse/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 from decouple import config, RepositoryEnv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,10 +89,9 @@ WSGI_APPLICATION = "Bitfuse.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        config("DATABASE_URL")
+    )
 }
 
 
